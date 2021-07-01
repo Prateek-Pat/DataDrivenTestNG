c2579b3690fff087296f98eb02431a0c7449a123
Prateek Yadav <pyprateekmax14@gmail.com>
2021-07-01 18:45:54 +0530

    DataFormatter

--- src/main/java/com/excel_reader_utility/ExcelUtility.java
+++ src/main/java/com/excel_reader_utility/ExcelUtility.java
@@ -35,12 +35,12 @@ public class ExcelUtility {
 		
 		for (int i=0;i<rowCount-1;i++) // this will traverse all the rows in FreeCrmData
 		{
-			row  = sheet.getRow(i+1);
+			row  = sheet.getRow(i+1); //this is capture the data from row 1, Since i=0 +i
 			for(int j=0;j<colCount;j++) // this will traverse the all cols in freeCrmData
 			{
 				
-			XSSFCell cell = row.getCell(j);
-			obj[i][j] = formatter.formatCellValue(cell);
+			XSSFCell cell = row.getCell(j); // Since i=0, it will iterate for every cell i.e column i.e 0,1,2
+			obj[i][j] = formatter.formatCellValue(cell); // formatter used to format the any data type in string
 			
 				/*String cellData = sheet.getRow(row).getCell(col).getStringCellValue();
 				obj[row-1][col]=cellData;*/
